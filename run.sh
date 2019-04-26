25be3d840184f05fb5f3386949a40b4d7f1badac
Andre Graf <andre@dergraf.org>
2019-04-26 14:13:11 +0200

    fix path

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ curl https://raw.githubusercontent.com/vernemq/vernemq/${VERNEMQ_VERSION:-master
 rebar3 get-deps
 rm -Rf _build
 rebar3 compile
-find -L _build/default -regex ".*\/\(ebin\|priv\)\/.*" -print0 | tar -czvf bundle.tar.gz --null -T -
+find -L _build/default/lib -regex ".*\/\(plugins\|ebin\|priv\)\/.*" -print0 | tar -czvf bundle.tar.gz --null -T -
 mkdir -p bundler
 mv bundle.tar.gz bundler/.
 erl -s inets -eval "{ok, _} = inets:start(httpd, [{port, ${HTTP_PORT:-8080}}, {document_root, \"$PWD/bundler\"}, {bind_address, \"${HTTP_ADDRESS:-0.0.0.0}\"}, {server_name, \"vmq_plugin_bundler\"}, {server_root, \"$PWD/bundler\"}])." -noshell -noinput
