68e4b2cca6f62b5100c9f82b6fb5f43a71bdab01
Lars Hesel Christensen <lars@larshesel.dk>
2019-06-06 10:32:00 +0200

    Tell the user where to fetch the bundle

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ rebar3 compile
 find -L _build/default/lib -regex ".*\/\(plugins\|ebin\|priv\)\/.*" -print0 | tar -czvf bundle.tar.gz --null -T -
 mkdir -p bundler
 mv bundle.tar.gz bundler/.
+echo "Serving bundle on http://${HTTP_ADDRESS:-0.0.0.0}:${HTTP_PORT:-8080}/bundle.tar.gz"
 erl -s inets -eval "{ok, _} = inets:start(httpd, [{port, ${HTTP_PORT:-8080}}, {document_root, \"$PWD/bundler\"}, {bind_address, \"${HTTP_ADDRESS:-0.0.0.0}\"}, {server_name, \"vmq_plugin_bundler\"}, {server_root, \"$PWD/bundler\"}])." -noshell -noinput
